cf4d3e0da401a6d1dea6b70981e887ba9b14c240
NicoMB2002 <nicomoncayo@hotmail.com>
2024-05-16 15:33:47 -0400

    created method isPostalCodeValid in Address class

--- src/main/java/org/example/Address.java
+++ src/main/java/org/example/Address.java
@@ -15,4 +15,31 @@ public class Address {
     private String province;
     private String postalCode;
     private String country;
+
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null) {
+            return false;
+        }
+        char[] postalCodeArray = postalCode.toCharArray();
+        int len = postalCodeArray.length;
+        if (len == 6) {
+            return Character.isLetter(postalCodeArray[0]) &&
+            Character.isLetter(postalCodeArray[2]) &&
+            Character.isLetter(postalCodeArray[4]) &&
+            Character.isDigit(postalCodeArray[1]) &&
+            Character.isDigit(postalCodeArray[3]) &&
+            Character.isDigit(postalCodeArray[5]);
+
+        }
+        if (len == 7) {
+            return Character.isLetter(postalCodeArray[0]) &&
+                    Character.isLetter(postalCodeArray[2]) &&
+                    Character.isLetter(postalCodeArray[5]) &&
+                    Character.isWhitespace(postalCodeArray[3]) &&
+                    Character.isDigit(postalCodeArray[1]) &&
+                    Character.isDigit(postalCodeArray[4]) &&
+                    Character.isDigit(postalCodeArray[6]);
+        }
+        return false;
+    }
 }
