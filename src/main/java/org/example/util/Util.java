99758b4b7e083a330927dc67c685290b37790faa
NicoMB2002 <nicomoncayo@hotmail.com>
2024-05-24 20:18:29 -0400

    created toTitleCase() in Util Class

--- src/main/java/org/example/util/Util.java
+++ src/main/java/org/example/util/Util.java
@@ -1,4 +1,21 @@
 package org.example.util;
 
 public class Util {
+    public static String toTitleCase(String strIn) {
+        if (strIn == null || strIn.isEmpty()) {
+            return strIn;
+        }
+
+        String[] words = strIn.split(" ");
+        String result = "";
+
+        for (String word : words) {
+            if (!result.isEmpty()) {
+                result += " ";
+            }
+            result += Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();
+        }
+
+        return result;
+    }
 }
