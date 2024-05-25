6ece7a5d8d79aebbf2d3b468cb69727e92d70c77
NicoMB2002 <nicomoncayo@hotmail.com>
2024-05-24 23:25:37 -0400

    Created Address, Department and Student in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Address address = new Address(8025, "Papineau", "Montreal", "Quebec",
                 "H2e 2h5", "Canada");
         System.out.println(address);
 
+        Department department = new Department("Biology");
+        System.out.println(department);
+
+        Student student = new Student("nicolas", Gender.MALE, address, department);
+        System.out.println(student);
+
+        Student student2 = new Student("Michael", Gender.MALE, address, department);
+        System.out.println(student2);
     }
 }
